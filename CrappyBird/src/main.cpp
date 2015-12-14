cd2a67f6bb6a6c50ade3f91f1d38871b73c24953
Dean Becker <dean.becker@nexusinds.com>
2015-12-14 16:00:26 +0000

    Initial Project creation; Linked GLEW and GLFW;

---
+++ CrappyBird/src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CrappyBird
+//
+//  Created by Dean Becker on 14/12/2015.
+//  Copyright © 2015 Dean Becker. All rights reserved.
+//
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
